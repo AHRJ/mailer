705412df58d7b4e93a3976f13c6974daf536fa5a
alexmisk <alexmisk@ya.ru>
2021-08-04 14:11:48 +0300

    chore(news_digest): translate app name in admin interface

--- news_digest/apps.py
+++ news_digest/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class NewsDigestConfig(AppConfig):
     name = "news_digest"
+    verbose_name = "Новостной дайджест"
+    verbose_name_plural = "Новостные дайджесты"
