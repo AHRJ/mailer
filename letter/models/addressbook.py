c0500f4e65a0edace5d7e47229e44fb99e5924d7
alexmisk <alexmisk@ya.ru>
2021-10-22 17:34:01 +0300

    feat(letter): add SendPulse import for addressbooks

--- letter/models/addressbook.py
+++ letter/models/addressbook.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from zzr_mailer.utils.sendpulse import SPSender
+
 
 class AddressBook(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
@@ -12,3 +14,16 @@ class AddressBook(models.Model):
     class Meta:
         verbose_name = "Адресная книга"
         verbose_name_plural = "Адресные книги"
+
+    @staticmethod
+    def load_from_sendpulse():
+        try:
+            addressbooks_dict = SPSender.get_list_of_addressbooks()
+            addressbooks = [
+                AddressBook(id=addressbook["id"], name=addressbook["name"])
+                for addressbook in addressbooks_dict
+            ]
+        except:  # noqa
+            addressbooks = []
+
+        AddressBook.objects.bulk_create(addressbooks, ignore_conflicts=True)
