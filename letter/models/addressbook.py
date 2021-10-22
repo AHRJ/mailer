4c74235b89956c476d2259c6deef3436a176c8a4
alexmisk <alexmisk@ya.ru>
2021-10-22 14:28:53 +0300

    feat(letter): add status field on addressbook model

--- letter/models/addressbook.py
+++ letter/models/addressbook.py
@@ -4,6 +4,7 @@ from django.db import models
 class AddressBook(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=255, default="Адресная книга")
+    status = models.BooleanField(verbose_name="Активная", default=True)
 
     def __str__(self):
         return self.name
