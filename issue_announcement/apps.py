df4248ef9de38f96cf0e914c74bd4cfb6536e8fa
alexmisk <alexmisk@ya.ru>
2021-10-29 10:26:26 +0300

    chore(content): add verbose name to issue_ann app

--- issue_announcement/apps.py
+++ issue_announcement/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class IssueAnnouncementConfig(AppConfig):
     name = "issue_announcement"
+    verbose_name = "Анонс номера"
+    verbose_name_plural = "Анонсы номера"
